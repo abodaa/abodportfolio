ef1d6c27d3666d2c8a673625f306a61af8c6f83d
abodaa <abrahambogale747@gmail.com>
2022-11-16 16:51:13 +0300

    First commit for CICD production

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,6 @@ import { gsap } from "gsap";
 import { NavLink } from "react-router-dom";
 import HashLoader from "react-spinners/HashLoader";
 
-
 import data from "../data.js";
 
 export default function Project() {
@@ -50,7 +49,7 @@ export default function Project() {
           <div className="project-description-container">
             <div>
               <p className="featured">Featured Project</p>
-              <h2 className="project-title">Abod Movies</h2>
+              <h2 className="project-title">Lekulu Entertainment</h2>
               <h3 className="project-subtitle">A Movie Streaming Website</h3>
             </div>
             <p className="project-description">
@@ -72,7 +71,7 @@ export default function Project() {
               </p>
               <a href="#" className="project-links">
                 <a
-                  href="https://abresh-movies.netlify.app/"
+                  href="https://lekuluent.com"
                   target="blank"
                   className="quick-view"
                 >
@@ -82,7 +81,7 @@ export default function Project() {
             </div>
             <div className="links">
               <a
-                href="https://abresh-movies.netlify.app/"
+                href="https://lekuluent.com"
                 target="blank"
                 className="project-links"
               >
